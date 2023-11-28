de187d0cabbda4f4ecc29eea6bd5fe170295af8f
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-11-27 21:59:39 -0800

    3 times a string

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+str_value = "Holberton School"
+
+# Print 3 times the value of str
+print(f"{str_value * 3}")
+
+# Print a new line
+print()
+
+# Print the 9 first characters of str
+print(f"{str_value[:9]}")
