c76a466d4dbe23e45c7d3643f0561dac1e6d4cf9
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-11-30 06:21:31 -0800

    simple function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-add_0 = __import__('add_0')
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+
+def main():
+    # Import the add function from add_0
+    from add_0 import add
+
+    # Define variables
+    a = 1
+    b = 2
+
+    # Perform addition
+    result = add(a, b)
+
+    # Display the result using formatted string
+    print("{:d} + {:d} = {:d}".format(a, b, result))
+
+if __name__ == "__main__":
+    # Call the main function when the script is executed
+    main()
