a5c80100e1834515824b7687fff553035c59ad6d
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-12-07 14:34:58 -0800

    Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Get the number of keys in the given dictionary.
+
+    Parameters:
+    - a_dictionary: input dictionary.
+
+    Returns:
+    The number of keys in dictionary.
+    """
+    return len(a_dictionary)
