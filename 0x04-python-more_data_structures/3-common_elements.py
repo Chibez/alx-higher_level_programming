d20339b8ddcf4fa623b32465e0e64de891d184f5
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-12-07 14:27:28 -0800

    in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Find the common elements between two sets.
+
+    Parameters:
+    - set_1: The first set.
+    - set_2: The second set.
+
+    Returns:
+    A new set containing the common elements.
+    """
+    return set_1 & set_2
