8d87b649daced0d5542b46a3f5efa9e763d946bf
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-12-19 16:29:54 -0800

    Square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
 
+"""Defines a square class"""
+
+
+class Square:
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """Initializes a new square.
+
+        Args:
+            size (int): The size of the square.
+
+        Returns:
+            None
+        """
+        # Using double underscores to make __size a private attribute
+        self.__size = size
