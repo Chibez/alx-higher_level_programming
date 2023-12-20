2673b539f8fed0bab98bf2fb880957f5e18a946b
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-12-19 17:15:40 -0800

    square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,11 +17,17 @@ class Square:
             ValueError: If size is less than zero.
         """
 
-        # Check if size is an integer and greater than or equal to zero
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
 
-        # Assign the size to the private attribute __size
         self.__size = size
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
+        return (self.__size ** 2)
