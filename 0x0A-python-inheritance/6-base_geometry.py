fea8c002c3411d13c830f4a25e0803d7bbf88f7d
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2024-01-08 15:54:33 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines the base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents a base geometry."""
+
+    def area(self):
+        """Method not implemented."""
+        raise Exception("area() is not implemented")
+
