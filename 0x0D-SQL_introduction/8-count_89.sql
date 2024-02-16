42ed72036db777506de399a5747f204e8a7fe062
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2024-02-15 18:28:48 -0800

    Count 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Displays the number of records with id = 89 in the table 'first_table' of the specified database in your MySQL server.
+-- The database name should be passed as an argument to the mysql command.
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
