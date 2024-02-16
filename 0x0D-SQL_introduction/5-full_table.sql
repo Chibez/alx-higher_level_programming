6820f49ba3491770805d303eae5d4a31788c14f1
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2024-02-15 18:24:00 -0800

    Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Prints the full description of the table 'first_table' from the specified database in your MySQL server.
+-- The database name should be passed as an argument to the mysql command.
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements.
+
+SHOW CREATE TABLE first_table;
