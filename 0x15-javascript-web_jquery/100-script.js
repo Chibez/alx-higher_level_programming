0baefcceea55a03368a7499f233616069c793a6e
caleb odobi <odocaleb@gmail.com>
2024-05-03 18:03:35 -0700

    script that updates the text color

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Select the <header> element using document.querySelector
+  var headerElement = document.querySelector('header');
+  
+  // Check if the <header> element is found
+  if (headerElement) {
+    // Set the text color of the <header> element to red (#FF0000)
+    headerElement.style.color = '#FF0000';
+  }
+});
+
